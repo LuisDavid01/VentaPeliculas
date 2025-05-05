4e57844ffd41c042229701c6e27c1ee526b4b9d4
Luis David <luisdavidmirandav12@gmail.com>
2025-05-05 14:10:30 -0600

    crud tipo de sala

--- api/services/AuthService.js
+++ api/services/AuthService.js
@@ -22,7 +22,6 @@ class AuthService
 		if(data.username == null || data.password == null) throw new Error("Contraseña o usuario vacio");
 		
 		const userLookUp = await usuariosService.getUsuarioByUsername(data.username);
-		console.log(userLookUp);
 		const password = data.password.toString();
 		const verifyPassword = await securityService.CheckPassword(password, userLookUp.password);
 		if( verifyPassword === true){
@@ -31,7 +30,6 @@ class AuthService
 				{
 					expiresIn:'20m'
 				});
-			console.log(token);
 			userLookUp.token = token;
 			await usuariosService.updateLoggedUsuario(userLookUp._id,userLookUp);
 			return userLookUp;
---
+++ ventaPeliculaWeb/ventaPeliculaWeb/Controllers/TipoSalaController.cs
@@ -0,0 +1,106 @@
+﻿using System.Net.Http.Headers;
+using AgendaTuLookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using ventaPeliculaWeb.Models;
+
+namespace ventaPeliculaWeb.Controllers
+{
+    [FiltroSesion]
+    [FiltroAdmin]
+    public class TipoSalaController : Controller
+    {
+        private readonly IHttpClientFactory _httpClient;
+        private readonly IConfiguration _configuration;
+
+        public TipoSalaController(IConfiguration configuration, IHttpClientFactory httpClient)
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            using (var http = _httpClient.CreateClient())
+            {
+                var url = _configuration.GetSection("Variables:urlWebApi").Value + "TipoSala";
+                var response = http.GetAsync(url).Result;
+                if (response.IsSuccessStatusCode)
+                {
+
+                    var result = response.Content.ReadFromJsonAsync<List<TipoSalaModel>>().Result;
+
+
+
+                    return View(result);
+                }
+                return View();
+
+            }
+        }
+        public IActionResult CrearTipoSala()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CrearTipoSala(TipoSalaModel model)
+        {
+            using (var http = _httpClient.CreateClient())
+            {
+                var url = _configuration.GetSection("Variables:urlWebApi").Value + "TipoSala";
+                http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Request.Cookies["Token"]);
+                var response = http.PostAsJsonAsync(url, model).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index", "TipoSala");
+                }
+                return RedirectToAction("Index", "TipoSala");
+
+            }
+        }
+
+        public IActionResult VerTipoSala(string id)
+        {
+            using (var http = _httpClient.CreateClient())
+            {
+                var url = _configuration.GetSection("Variables:urlWebApi").Value + "TipoSala/" + id;
+                var response = http.GetAsync(url).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = response.Content.ReadFromJsonAsync<TipoSalaModel>().Result;
+
+                    return View(result);
+                }
+                return View(null);
+
+            }
+        }
+        [HttpPost]
+        public IActionResult EditarTipoSala(TipoSalaModel model)
+        {
+            using (var http = _httpClient.CreateClient())
+            {
+                var url = _configuration.GetSection("Variables:urlWebApi").Value + "TipoSala/" + model._id;
+                http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Request.Cookies["Token"]);
+                var response = http.PutAsJsonAsync(url, model).Result;
+
+                return RedirectToAction("Index", "TipoSala");
+
+            }
+        }
+
+        public IActionResult EliminarTipoSala(string id)
+        {
+
+            using (var http = _httpClient.CreateClient())
+            {
+                var url = _configuration.GetSection("Variables:urlWebApi").Value + "TipoSala/" + id;
+                http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Request.Cookies["Token"]);
+                var response = http.DeleteAsync(url).Result;
+
+                return RedirectToAction("Index", "TipoSala");
+
+            }
+        }
+
+    }
+}
