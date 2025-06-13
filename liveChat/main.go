ac894ccbe8413563dc16040402d56aa0eb8a4ef3
Luis David <luisdavidmirandav12@gmail.com>
2025-06-12 23:29:23 -0600

    Primera integracion con supabase

--- liveChat/main.go
+++ liveChat/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"os"
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Si... Hare un chat con golang!")
+	if err := godotenv.Load(); err != nil {
+        log.Fatal("Error cargando el archivo .env")
+    }
+
+    dbURL := os.Getenv("POSTGRES_URL")
+
+	conn, err := pgx.Connect(context.Background(), dbURL)
+	if err != nil {
+		log.Fatalf("Failed to connect to the database: %v", err)
+	}
+	defer conn.Close(context.Background())
+
+	// Example query to test connection
+	var version string
+	if err := conn.QueryRow(context.Background(), "SELECT version()").Scan(&version); err != nil {
+		log.Fatalf("Query failed: %v", err)
+	}
+
+	log.Println("Connected to:", version)
 }
